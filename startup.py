1ef9fff7ecd9200f0f65932ccea4105a3bea58de
Goku0127 <g9682430@gmail.com>
2025-02-05 19:01:52 +0530

    Create startup.py

---
+++ startup.py
@@ -0,0 +1,12 @@
+
+echo "
+            ┏┳┓╋┏┓╋╋╋╋┏┓┏┓
+            ┃┃┣┓┃┗┳┳┳━╋╋┛┃
+            ┃┃┃┗┫┏┫┏┫╋┃┃╋┃
+            ┗━┻━┻━┻┛┗━┻┻━┛
+
+      Visit @goku_groupz for updates!!
+
+"
+if [ -f .env ] ; then  set -o allexport; source .env; set +o allexport ; fi
+python3 -m gulfam ; fi
